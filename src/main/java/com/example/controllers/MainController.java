0c927a6991360b15aa7b756abc89b630bfbf46c1
criscgv7 <criscgv7@gmail.com>
2023-03-21 17:23:11 +0100

    Otrotemporal

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.entities.Estudiante;
 import com.example.entities.Facultad;
@@ -52,7 +54,15 @@ public class MainController {
 
 
         return "views/formularioAltaEstudiante"; 
+
     }
+
+    @PostMapping ("/altaEstudiante")
+    public String altaEstudiante(){
+
+       return "redirect:/listar";
+    }
+
 
     
 }
