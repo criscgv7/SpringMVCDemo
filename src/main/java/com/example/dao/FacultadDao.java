a21ff8644e136ef2b6d02d9cb69c291fe49403e2
criscgv7 <criscgv7@gmail.com>
2023-03-21 10:34:24 +0100

    cambioa

--- src/main/java/com/example/dao/FacultadDao.java
+++ src/main/java/com/example/dao/FacultadDao.java
@@ -2,9 +2,6 @@ package com.example.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/*jajaja */
-import org.springframework.stereotype.Repository;
-
 import com.example.entities.Facultad;
 
 
