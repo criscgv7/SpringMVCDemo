d48760f80aeec8742483827e02e9d3af2d24c0c0
criscgv7 <criscgv7@gmail.com>
2023-03-22 09:59:39 +0100

    Cambios

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,21 +1,22 @@
 package com.example.controllers;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.entities.Estudiante;
 import com.example.entities.Facultad;
 import com.example.services.EstudianteService;
 import com.example.services.FacultadService;
-
 
 
 @Controller
@@ -25,11 +26,12 @@ import com.example.services.FacultadService;
 // Verbs: Accion mediante la cual llega la informacion viajando a traves del
 // protocolo http: put, delete, option, get, post...
 public class MainController {
-    @Autowired
- private EstudianteService estudianteService;
-@Autowired
- private FacultadService facultadService; 
 
+    private static final Logger LOG= Logger.getLogger("MainController"); 
+    @Autowired
+    private EstudianteService estudianteService;
+    @Autowired
+    private FacultadService facultadService;
 
     /**
      * El metodo siguiente devuelve un listado de estudiantes
@@ -38,31 +40,31 @@ public class MainController {
     @GetMapping("/listar")
     public ModelAndView listar() {
 
-        List <Estudiante> estudiantes = estudianteService.findAll(); 
+        List<Estudiante> estudiantes = estudianteService.findAll();
 
         ModelAndView mav = new ModelAndView("views/listarEstudiantes");
         mav.addObject("estudiantes", estudiantes);
         return mav;
     }
-@GetMapping("/frm")
-    public String formularioAltaEstudiante(Model model){
 
-        List <Facultad> facultades =facultadService.findAll(); 
-       
-        model.addAttribute("estudiante", new Estudiante()); 
-        model.addAttribute("facultades", facultades); 
+    @GetMapping("/frm")
+    public String formularioAltaEstudiante(Model model) {
 
+        List<Facultad> facultades = facultadService.findAll();
 
-        return "views/formularioAltaEstudiante"; 
+        model.addAttribute("estudiante", new Estudiante());
+        model.addAttribute("facultades", facultades);
+
+        return "views/formularioAltaEstudiante";
 
     }
 
-    @PostMapping ("/altaEstudiante")
-    public String altaEstudiante(){
+    @PostMapping("/altaEstudiante")
+    public String altaEstudiante(@ModelAttribute Estudiante estudiante, @RequestParam(name="numerosTelefonos") String telefonosRecibidos) {
+        LOG.info("Telefonos recibidos:" + telefonosRecibidos);
+estudianteService.save(estudiante); 
 
-       return "redirect:/listar";
+        return "redirect:/listar";
     }
 
-
-    
 }
